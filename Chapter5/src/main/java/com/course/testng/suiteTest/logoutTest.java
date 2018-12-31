35a72640efc21fc3cd454e610f7e76d468db8531
ganl1011 <46258808+ganl1011@users.noreply.github.com>
2019-01-01 00:38:35 +0800

    Create logoutTest.java

---
+++ Chapter5/src/main/java/com/course/testng/suiteTest/logoutTest.java
@@ -0,0 +1,27 @@
+package com.course.testng.suiteTest;
+
+import org.testng.annotations.*;
+
+public class logoutTest {
+//    @BeforeSuite
+//    public void beforeSuite(){
+//        System.out.println("logoutBeforeSuit");
+//    }
+//    @BeforeTest
+//    public void loginBefore(){
+//        System.out.println("logoutBefore");
+//    }
+    @Test
+    public void login(){
+        System.out.println("logout");
+    }
+//    @AfterTest
+//    public void loginAfter(){
+//        System.out.println("logoutAfter");
+//    }
+//    @AfterSuite
+//    public void afterSuite(){
+//        System.out.println("logoutAfterSuit");
+//    }
+
+}
